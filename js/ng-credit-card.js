274f0175827a704a7507ed32360180f210e1a4a0
Michael Liu <michael.liu95@gmail.com>
2015-04-28 11:54:54 -0500

    Fix minimum month

--- js/ng-credit-card.js
+++ js/ng-credit-card.js
@@ -59,7 +59,7 @@ angular.module("ngCreditCard", [])
                 "<input ng-model='card' name='cc_card' ng-change='updateNumber()' class='creditcard-number' type='text' placeholder='&#9679;&#9679;&#9679;&#9679;&nbsp;&#9679;&#9679;&#9679;&#9679;&nbsp;&#9679;&#9679;&#9679;&#9679;&nbsp;&#9679;&#9679;&#9679;&#9679;&nbsp;' maxlength='19' ng-model-options='{allowInvalid: true}' ng-trim='false' required />" +
                 "<input ng-model='cvc' name='cc_cvc' class='creditcard-cvc' type='number' placeholder='CVC' maxlength='4' />" +
                 "<input ng-model='cardHolderName' name='cc_name' class='creditcard-name' type='text' placeholder='John Smith' required />" +
-                "<input ng-model='expMonth' name='cc_month' class='creditcard-exp-month' type='number' placeholder='MM' min='0' max='12' maxlength='2' required />" +
+                "<input ng-model='expMonth' name='cc_month' class='creditcard-exp-month' type='number' placeholder='MM' min='1' max='12' maxlength='2' required />" +
                 "<input ng-model='expYear' name='cc_year' class='creditcard-exp-year' type='number' placeholder='YYYY' maxlength='4' required />" +
                 "</div>"
         }
